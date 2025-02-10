7a84aff5ae76119a0c9b4316aecd383c3b587d3a
Rohan Raj <rohan.raj@nixorcollege.edu.pk>
2025-02-10 18:31:26 +0100

    updated

--- io.openems.edge.smartsolarbox.modbus/src/io/openems/edge/smartsolarbox/modbus/SolarModbusMasterImpl.java
+++ io.openems.edge.smartsolarbox.modbus/src/io/openems/edge/smartsolarbox/modbus/SolarModbusMasterImpl.java
@@ -1,50 +1,17 @@
 package io.openems.edge.smartsolarbox.modbus;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Timestamp;
+import java.io.*;
 import java.util.Date;
 
-import org.osgi.service.cm.ConfigurationAdmin;
-import org.osgi.service.component.ComponentContext;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
-import org.osgi.service.metatype.annotations.Designate;
+import javax.sql.rowset.serial.SerialRef;
 
-import com.intelligt.modbus.jlibmodbus.Modbus;
-import com.intelligt.modbus.jlibmodbus.master.ModbusMaster;
-import com.intelligt.modbus.jlibmodbus.master.ModbusMasterFactory;
-import com.intelligt.modbus.jlibmodbus.serial.SerialParameters;
-import com.intelligt.modbus.jlibmodbus.serial.SerialPort;
-import com.intelligt.modbus.jlibmodbus.serial.SerialPortFactoryJSSC;
-import com.intelligt.modbus.jlibmodbus.serial.SerialUtils;
-
-import io.openems.common.exceptions.OpenemsException;
+import com.ghgande.j2mod.modbus.Modbus;
+import com.ghgande.j2mod.modbus.facade.ModbusSerialMaster;
+import com.ghgande.j2mod.modbus.util.SerialParameters;
 import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
-import io.openems.edge.bridge.modbus.api.BridgeModbus;
-import io.openems.edge.bridge.modbus.api.ModbusComponent;
-import io.openems.edge.bridge.modbus.api.ModbusProtocol;
-import io.openems.edge.common.component.OpenemsComponent;
+import java.sql.Timestamp;
+import gnu.io.SerialPort;
 
-@Designate(ocd = Config.class, factory = true)
-@Component(//
-		name = "SolarModbus.Master", //
-		immediate = true, //
-		configurationPolicy = ConfigurationPolicy.REQUIRE //
-)
-public class SolarModbusMasterImpl extends AbstractOpenemsModbusComponent implements SolarModbusMaster, ModbusComponent, OpenemsComponent {
-
-	@Reference
-	private ConfigurationAdmin cm;
+public class SolarModbusMasterImpl extends AbstractOpenemsModbusComponent {
 	
 	/*
 	 * STILL TO BE IMPLEMENTED:
@@ -52,10 +19,16 @@ public class SolarModbusMasterImpl extends AbstractOpenemsModbusComponent implem
 	 * Function to read array and battery status
 	 * 
 	 * */
-    final public SerialParameters sp = new SerialParameters(); //used to set baudrate, port, data bits, parity bits, etc
-    final public ModbusMaster m = ModbusMasterFactory.createModbusMasterRTU(sp); //used to connect to the other modbus device
+	
+	
+    final public SerialParameters sp; //used to set baudrate, port, data bits, parity bits, etc
+    final public ModbusSerialMaster m; //used to connect to the other modbus device
+    
+    final private String logFile = "/home/pi/Desktop/ecowet/smartsolar/solarlog.log";
     
 	final private static int slaveId = 1;
+	private int quantity = 0; //quantity of registers to read via ModBus
+	private int offset = 0x00; //address offset for register
 	private float arrayCurrent = 0; //input array current in A
 	private float arrayVoltage = 0; //input array voltage in V
 	private float arrayPower = 0; //input array power in W
@@ -69,55 +42,72 @@ public class SolarModbusMasterImpl extends AbstractOpenemsModbusComponent implem
 	private float loadVoltage = 0; // load voltage in V
 	private float loadPower = 0; // load power in W
 	private float deviceTemp = 0; // controller temperature in C
+	private String device = "/dev/ttyXRUSB0"; // can change through the method setDeviceName(), i'll leave the default here
 
-	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
-	protected void setModbus(BridgeModbus modbus) {
-		super.setModbus(modbus);
-	}
-
-	private Config config = null;
-
-	public SolarModbusMasterImpl() throws Exception {
-		super(//
-				OpenemsComponent.ChannelId.values(), //
-				ModbusComponent.ChannelId.values(), //
-				SolarModbusMaster.ChannelId.values() //
-		);
-		sp.setDevice(config.device());
+	public SolarModbusMasterImpl() throws Exception{ //calling all necessary functions in the constructor of the ModbusMaster
+		
+		sp = new SerialParameters();
+        sp.setPortName(device);
+        sp.setBaudRate(115200);
+        sp.setDatabits(8);
+        sp.setParity(SerialPort.PARITY_NONE);
+        sp.setStopbits(1);
+        
+        SerialRef.setSerialPortFactory(new SerialPortFactoryJSSC());
+        
+        Modbus.setLogLevel(Modbus.LogLevel.LEVEL_DEBUG);
+        m = ModbusMasterFactory.createModbusMasterRTU(sp);
 	}
 	
-	public SolarModbusMasterImpl(String argDev) throws Exception { //use this constructor to change the device name		
-		super(//
-				OpenemsComponent.ChannelId.values(), //
-				ModbusComponent.ChannelId.values(), //
-				SolarModbusMaster.ChannelId.values() //
-		);
-		sp.setDevice(argDev);
+	public SolarModbusMasterImpl(String argDev) throws Exception{ //use this constructor to change the device name
+		this.setDeviceName(argDev);
+		
+		sp = new SerialParameters();
+        sp.setPortName(device);
+        sp.setBaudRate(115200);
+        sp.setDatabits(8);
+        sp.setParity(SerialPort.PARITY_NONE);
+        sp.setStopbits(1);
+
+//        SerialRef.setSerialPortFactory(new SerialPortFactoryJSSC());
+
+//        Modbus.setLogLevel(Modbus.LogLevel.LEVEL_DEBUG);
+        m = ModbusMasterFactory.createModbusMasterRTU(sp);
 	}
-
-	@Activate
-	private void activate(ComponentContext context, Config config) throws OpenemsException {
-		if(super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm, "Modbus",
-				config.modbus_id())) {
-			return;
-		}
-		this.config = config;
-		sp.setBaudRate(SerialPort.BaudRate.BAUD_RATE_115200);
-        sp.setDataBits(8);
-        sp.setParity(SerialPort.Parity.NONE);
-        sp.setStopBits(1);
-
-        SerialUtils.setSerialPortFactory(new SerialPortFactoryJSSC());
-
-        Modbus.setLogLevel(Modbus.LogLevel.LEVEL_DEBUG);
-	}
-
+	
 	@Override
-	@Deactivate
-	protected void deactivate() {
-		super.deactivate();
-	}
-
+	protected ModbusProtocol defineModbusProtocol() {
+		return new ModbusProtocol(this, //
+				new FC4ReadInputRegistersTask(0x3101, Priority.HIGH,
+						m(SolarModbusMaster.ChannelId.ARRAY_CURRENT,
+								new SignedDoublewordElement(0x3101).wordOrder(WordOrder.LSWMSW)),
+						m(ElectricityMeter.ChannelId.ACTIVE_POWER,
+								new SignedDoublewordElement(3502).wordOrder(WordOrder.LSWMSW)),
+						m(SolarModbusMaster.ChannelId.ARRAY_VOLTAGE,
+								new SignedDoublewordElement(0x3100).wordOrder(WordOrder.LSWMSW)),
+						m(ElectricityMeter.ChannelId.VOLTAGE, new SignedWordElement(3506), SCALE_FACTOR_3),
+						m(SolarModbusMaster.ChannelId.ARRAY_POWER, new SignedWordElement(0x3102), SCALE_FACTOR_2),
+						m(ElectricityMeter.ChannelId.ACTIVE_PRODUCTION_ENERGY,
+								new SignedDoublewordElement(3508).wordOrder(WordOrder.LSWMSW)),
+						m(SolarModbusMaster.ChannelId.BOX_TEMP,
+								new SignedDoublewordElement(0x3110).wordOrder(WordOrder.LSWMSW)),
+						m(SolarModbusMaster.ChannelId.BATTERY_CURRENT,
+								new SignedDoublewordElement(0x331B).wordOrder(WordOrder.LSWMSW)),
+						m(SolarModbusMaster.ChannelId.BATTERY_VOLTAGE,
+								new SignedDoublewordElement(0x331A).wordOrder(WordOrder.LSWMSW)),
+						m(SolarModbusMaster.ChannelId.BATTERY_SOC,
+								new SignedDoublewordElement(0x311A).wordOrder(WordOrder.LSWMSW)),
+						m(SolarModbusMaster.ChannelId.LOAD_CURRENT,
+								new SignedDoublewordElement(0x310D).wordOrder(WordOrder.LSWMSW)),
+						m(ElectricityMeter.ChannelId.ACTIVE_CONSUMPTION_ENERGY,
+								new SignedDoublewordElement(3520).wordOrder(WordOrder.LSWMSW)),
+						m(SolarModbusMaster.ChannelId.LOAD_VOLTAGE,
+								new SignedDoublewordElement(0x310C).wordOrder(WordOrder.LSWMSW)),
+						m(SolarModbusMaster.ChannelId.LOAD_POWER,
+								new SignedDoublewordElement(0x310E).wordOrder(WordOrder.LSWMSW)),
+						m(SolarModbusMaster.ChannelId.DEVICE_TEMP,
+								new SignedDoublewordElement(0x3111).wordOrder(WordOrder.LSWMSW))));
+	
 	public int bool2Int(boolean[] bools) { //needed to convert from readDiscreteInputs
 		int intVal = 0;
 		for(int i = 0; i < bools.length; i++) {
@@ -128,119 +118,6 @@ public class SolarModbusMasterImpl extends AbstractOpenemsModbusComponent implem
 		return intVal;
 	}
 	
-    public float readArrayCurrent(ModbusMaster m) throws Exception {
-    	float arrayCurrent = 0;
-    	int quantity = 1;
-		int offset = 0x3101;
-		System.out.println("Requesting array current... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity);
-		arrayCurrent = registerReadValues[0] / (float)100; //TODO: verify if temp is also multiplied by 100
-    	return arrayCurrent;
-    }
-    
-    public float readArrayVoltage(ModbusMaster m) throws Exception{
-    	float arrayVoltage = 0;
-    	int quantity = 1;
-		int offset = 0x3100;
-		System.out.println("Requesting array voltage... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity);
-		arrayVoltage = registerReadValues[0] / (float)100; //TODO: verify if temp is also multiplied by 100
-    	return arrayVoltage;
-    }
-	
-	public float readArrayPower(ModbusMaster m) throws Exception {
-		float arrayPower = 0;
-		int quantity = 2; //from manual
-		int offset = 0x3102; //from manual
-		System.out.println("Requesting array power... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity); //function code 0x04 in jlibmodbus
-		registerReadValues[0] = registerReadValues[0] | (registerReadValues[1] << 16); //requires bit shifting because we read 2 registers: 1st is Lower Bits, 2nd is Higher Bits
-		arrayPower = registerReadValues[0] / (float)100; //divided by 100 because manual indicates so
-		return arrayPower;
-	}
-	
-    public float readBoxTemp(ModbusMaster m) throws Exception{
-    	float boxTemp = 0;
-    	int quantity = 1;
-		int offset = 0x3110;
-		System.out.println("Requesting box temperature... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity);
-		boxTemp = registerReadValues[0] / (float)100; //TODO: verify if temp is also multiplied by 100
-    	return boxTemp;
-    }
-    
-    public float readBatteryVoltage(ModbusMaster m) throws Exception{
-    	float batteryVoltage = 0;
-    	int quantity = 1;
-		int offset = 0x331A;
-		System.out.println("Requesting battery voltage... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity);
-		batteryVoltage = registerReadValues[0] / (float)100; //TODO: verify if temp is also multiplied by 100
-    	return batteryVoltage;
-    }
-    
-	public float readBatteryCurrent(ModbusMaster m) throws Exception {
-		float batteryCurrent = 0;
-		int quantity = 2; //from manual
-		int offset = 0x331B; //from manual
-		System.out.println("Requesting battery current... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity); //function code 0x04 in jlibmodbus
-		registerReadValues[0] = registerReadValues[0] | (registerReadValues[1] << 16); //requires bit shifting because we read 2 registers: 1st is Lower Bits, 2nd is Higher Bits
-		batteryCurrent = registerReadValues[0] / (float)100; //divided by 100 because manual indicates so
-		return batteryCurrent;
-	}
-    
-    public int readBatterySOC(ModbusMaster m) throws Exception{
-    	int batterySOC = 0;
-    	int quantity = 1;
-		int offset = 0x311A;
-		System.out.println("Requesting battery SOC... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity);
-		batterySOC = registerReadValues[0] / 1; //TODO: verify if SOC is also multiplied by 100
-    	return batterySOC;
-    }
-    
-    public float readLoadVoltage(ModbusMaster m) throws Exception{
-    	float loadVoltage = 0;
-    	int quantity = 1;
-		int offset = 0x310C;
-		System.out.println("Requesting load voltage... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity);
-		loadVoltage = registerReadValues[0] / (float)100; //TODO: verify if temp is also multiplied by 100
-    	return loadVoltage;
-    }
-    
-    public float readLoadCurrent(ModbusMaster m) throws Exception{
-    	float loadCurrent = 0;
-    	int quantity = 1;
-		int offset = 0x310D;
-		System.out.println("Requesting load current... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity);
-		loadCurrent = registerReadValues[0] / (float)100; //TODO: verify if temp is also multiplied by 100
-    	return loadCurrent;
-    }
-    
-    public float readLoadPower(ModbusMaster m) throws Exception{
-    	float loadPower = 0;
-    	int quantity = 2;
-		int offset = 0x310E;
-		System.out.println("Requesting load power... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity);
-		registerReadValues[0] = registerReadValues[0] | (registerReadValues[1] << 16);
-		loadPower = registerReadValues[0] / (float)100; //divided by 100 because manual indicates so
-    	return loadPower;    	
-    }
-    
-    public float readDeviceTemp(ModbusMaster m) throws Exception{
-    	float deviceTemp = 0;
-    	int quantity = 1;
-    	int offset = 0x3111;
-		System.out.println("Requesting device temperature... ");
-		int[] registerReadValues = m.readInputRegisters(slaveId, offset, quantity);
-		deviceTemp = registerReadValues[0] / (float)100; //TODO: verify if SOC is also multiplied by 100
-    	return deviceTemp;    	
-    }
-    
     public float getArrayCurrent() {
     	return this.arrayCurrent;
     }
@@ -293,15 +170,19 @@ public class SolarModbusMasterImpl extends AbstractOpenemsModbusComponent implem
     	return this.loadPower;
     }
     
+    public void setDeviceName(String argDevice) {
+    	this.device = argDevice;
+    }
+    
     public String getDeviceName() {
-    	return sp.getDevice();
+    	return this.device;
     }
     
     public static int getSlaveId() {
     	return slaveId;
     }
     
-    public void readValues(ModbusMaster m) throws Exception{
+    public void readValues(ModbusSerialMaster m) throws Exception{
     	this.arrayCurrent = readArrayCurrent(m);
     	this.arrayVoltage = readArrayVoltage(m);
     	this.arrayPower = readArrayPower(m);
@@ -314,18 +195,12 @@ public class SolarModbusMasterImpl extends AbstractOpenemsModbusComponent implem
     	this.loadVoltage = readLoadVoltage(m);
     	this.loadPower = readLoadPower(m);
     }
-	
-	@Override
-	protected ModbusProtocol defineModbusProtocol() throws OpenemsException {
-		// TODO implement ModbusProtocol
-		return new ModbusProtocol(this);
-	}
 
-	@Override
-	public String debugLog() {
+    //Function for logging to file
+	public void toLog() {
 		Date date = new Date();
 		Timestamp tsp = new Timestamp(date.getTime());
-		File file = new File(this.config.logFile());
+		File file = new File(this.logFile);
 		FileWriter fr = null;
 		BufferedWriter br = null;
 		PrintWriter pr = null;
@@ -356,6 +231,5 @@ public class SolarModbusMasterImpl extends AbstractOpenemsModbusComponent implem
 				e.printStackTrace();
 			}
 		}
-		return null;
 	}
 }
